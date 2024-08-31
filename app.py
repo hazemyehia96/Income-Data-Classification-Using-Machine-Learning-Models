db6b699d56e48a85d84e69e65d9b5ae548a119da
hazemyehia96 <142757526+hazemyehia96@users.noreply.github.com>
2024-09-01 00:21:17 +0300

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import streamlit as st
 import numpy as np
 import pandas as pd
 import joblib
-import scikit-learn
+import sklearn
 
 # Load the trained model
 model = joblib.load(r"logistic_regression.joblib")
